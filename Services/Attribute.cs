250bd728c417daf5cd9314b778146e349e33f524
milad ashoori <milad.ashoori@gmail.com>
2021-11-12 05:48:07 -0800

    add attribute and category services

---
+++ Services/Attribute.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce_backend.Data;
+using eCommerce_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace eCommerce_backend.Services
+{
+    public class AttributeService:Base.BaseService<Attribute>
+    {
+        public AttributeService(DbSet<Attribute> ts,ECommerceContext context):base(ts,context) {
+
+        }
+
+    }
+}
---
+++ Services/Category.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce_backend.Data;
+using eCommerce_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace eCommerce_backend.Services
+{
+    public class CategoryService : Base.BaseService<Category>
+    {
+        public CategoryService(DbSet<Category> ts,ECommerceContext context):base(ts,context) {
+
+        }
+
+    }
+}
