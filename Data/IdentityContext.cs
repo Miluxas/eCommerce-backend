4761e19458c5eb37a9ed3592ba4cbe2e2b797797
milad ashoori <milad.ashoori@gmail.com>
2021-11-21 01:55:14 -0800

    add ASP.NET user and roles and ...

---
+++ Data/IdentityContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using eCommerce_backend.Models;
+using eCommerce_backend.Base;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace eCommerce_backend.Data
+{
+
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options):base(options)
+        { }
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace eCommerce_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.IdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddDbContext<Data.ECommerceContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllers();
         }
