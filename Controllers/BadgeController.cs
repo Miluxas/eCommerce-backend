35eb1a749ae74098804f03eb63f04d2691a76cb3
milad ashoori <milad.ashoori@gmail.com>
2021-12-17 07:00:28 -0800

    add badge controller

---
+++ Controllers/BadgeController.cs
@@ -0,0 +1,19 @@
+﻿using eCommerce_backend.Data;
+using eCommerce_backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace eCommerce_backend.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class BadgeController : Base.BaseController<Badge>
+    {
+        public BadgeController(ECommerceContext context, Guid? userID = null) : base(userID)
+        {
+            service = new Services.BadgeService(context.Badges, context);
+        }
+    }
+}
