1154a886f458cc6b34046f9cb5d395ccb96ccebe
milad ashoori <milad.ashoori@gmail.com>
2021-12-26 08:54:19 -0800

    add order controller  and service

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using eCommerce_backend.Data;
+using eCommerce_backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace eCommerce_backend.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : Base.BaseController<Order>
+    {
+        public OrderController(ECommerceContext context, Guid? userID = null) : base(userID)
+        {
+            service = new Services.OrderService(context.Orders, context);
+        }
+    }
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce_backend.Data;
+using eCommerce_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace eCommerce_backend.Services
+{
+    public class OrderService : Base.BaseService<Order>
+    {
+        public OrderService(DbSet<Order> ts,ECommerceContext context):base(ts,context) {
+
+        }
+
+    }
+}
